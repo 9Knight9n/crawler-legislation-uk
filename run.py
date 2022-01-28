f8d40f2325ab04e31831032547f479dd8220fbcd
Sajad <naghizadehmdsd@gmail.com>
2022-01-28 16:35:40 +0330

    added functionality to save in excel and download files

--- run.py
+++ run.py
@@ -4,17 +4,16 @@ from extract.act_list import get_act_list_single_page
 
 
 page = 1
+count=0
 
 while True:
-    print(f'getting all acts in page {page}...')
     acts = get_act_list_single_page(page)
-    print(f'found {len(acts)} acts in page {page}.')
     for index_,act in enumerate(acts):
-        print(f'processing act {index_+1} in page {page}.')
+        count+=1
         append_act(act)
     if page > 10:
         break
     if len(acts)==0:
         break
     page+=1
-    print("---------------------------------------------")
+    print(f'total processed acts :{count} .')
