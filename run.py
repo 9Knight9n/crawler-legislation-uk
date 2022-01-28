5dffaf2510cbdf1d6dedfa7c8ca3409341b7ab65
Sajad <naghizadehmdsd@gmail.com>
2022-01-28 17:09:37 +0330

    now we will continue from where we left in case of an interruption

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
+from save import base_dir
 from save.act import append_act
 from extract.act_list import get_act_list_single_page
+import os
 
+page_file_dir = base_dir+'/pages_loaded.txt'
+if not os.path.isfile(page_file_dir):
+    f = open(page_file_dir, "w")
+    f.write(str(1)+"\n")
+    f.close()
+file = open(page_file_dir,"r")
+page = file.readline()
+file.close()
+page = int(page)
 
-
-page = 1
 count=0
 
 while True:
@@ -11,9 +20,13 @@ while True:
     for index_,act in enumerate(acts):
         count+=1
         append_act(act)
+        break
     if page > 10:
         break
     if len(acts)==0:
         break
     page+=1
-    print(f'total processed acts :{count} .')
+    f = open(page_file_dir, "w")
+    f.write(str(page) + "\n")
+    f.close()
+    print(f'total processed acts :{count}')
