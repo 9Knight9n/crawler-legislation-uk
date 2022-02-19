cea32a16a259f8b91b455731d20a154439b444c2
Sajad <naghizadehmdsd@gmail.com>
2022-02-19 18:08:53 +0330

    colab

--- run.py
+++ run.py
@@ -10,12 +10,15 @@ from openpyxl import load_workbook
 from utils import trim
 
 urls=[
-    "ukpga",#320
-    "ukla",#60
-    "ukcm",#40
-    "ukci",#20
-    "uksro",#60
-    "uksi",#500
+    "ukpga",
+    "ukla",
+    "ukcm",
+    "ukci",
+    "uksro",
+    "uksi",
+]
+urls_max=[
+    260,60,40,20,60,560
 ]
 
 excel = pd.read_excel(skipped_files_list_dir,)
@@ -55,30 +58,9 @@ for index,url in enumerate(urls):
                     ws.append([trim(base_url+act)])
                     wb.save(skipped_files_list_dir)
                 count += 1
-                if index==0:
-                    if count == 320:
-                        stored_exception = "None"
-                        break
-                elif index==1:
-                    if count == 60:
-                        stored_exception = "None"
-                        break
-                elif index==2:
-                    if count == 40:
-                        stored_exception = "None"
-                        break
-                elif index==3:
-                    if count == 20:
-                        stored_exception = "None"
-                        break
-                elif index==4:
-                    if count == 60:
-                        stored_exception = "None"
-                        break
-                elif index==5:
-                    if count == 500:
-                        stored_exception = "None"
-                        break
+                if count == urls_max[index_]:
+                    stored_exception = "None"
+                    break
             except KeyboardInterrupt or SystemExit:
                 stored_exception = sys.exc_info()
             except:
@@ -88,12 +70,6 @@ for index,url in enumerate(urls):
                 continue
         if stored_exception:
             break
-            # break
-        # break
-        # if page > 10:
-        #     break
-        # if count > 100:
-        #     break
         if len(acts) == 0:
             break
         page += 1
@@ -107,8 +83,7 @@ for index,url in enumerate(urls):
         print(f'total processed acts :{count}')
 
     if stored_exception:
-        if stored_exception != "None":
-            raise stored_exception[0](stored_exception[1]).with_traceback(stored_exception[2])
+        print("Either max act count limit reached or user stopped the process!")
 
     print(f"finished fetching '{base_url}{url}' ...")
 
